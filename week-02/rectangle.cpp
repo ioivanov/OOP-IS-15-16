b18563b0d73242e860e074708990c104cf212a6b
Ivailo Ivanov <ivaylo.ivanov.95@hotmail.com>
2016-03-04 11:20:44 +0200

    Решение на задача Х.

---
+++ week-02/rectangle.cpp
@@ -0,0 +1,58 @@
+#include <iostream.h>
+
+struct Rectangle
+{
+    int length; // дължина
+    int width; // широчина
+};
+
+////точка 1
+void print_rectangle(Rectangle r)
+{
+    cout << r.length << r.width << endl;
+}
+
+////точка 2
+void init_rectangle(Rectangle& r)
+{
+    do
+    {
+        cout << "length: "; 
+        cin >> r.length;
+        cout << "width: ";
+        cin >> r.width;
+    } 
+    while(r.length <= 0 || r.width <= 0);
+}
+
+////точка 3
+Rectangle init_rectangle()
+{
+    Rectangle r;
+    
+    do
+    {
+        cout << "length: ";
+        cin >> r.length;
+        cout << "width: "; 
+        cin >> r.width;
+    } 
+    while(r.length <= 0 || r.width <= 0);
+
+    return r;
+}
+
+int main()
+{
+    Rectangle r;
+
+    r = init_rectangle();
+
+    print_rectangle(r);
+
+    init_rectangle(r);
+
+    print_rectangle(r);
+
+    return 0;
+}
